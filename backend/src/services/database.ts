ee91d7c9c182690c07feb9c8205c255097aebfda
split73 <54321lexa12345@mail.ru>
2026-09-09 19:43:13 +0400

    vercel debug

--- backend/src/services/database.ts
+++ backend/src/services/database.ts
@@ -9,7 +9,7 @@ class Database {
     // const cleanConnectionString = config.databaseUrl.replace(/&?channel_binding=[^&]+/, '');
 
     this.pool = new Pool({
-      connectionString: "postgresql://neondb_owner:npg_gPtB4VWLTf0r@ep-withered-shape-av6m9ydq-pooler.c-11.us-east-1.aws.neon.tech/neondb?sslmode=require",
+      connectionString: "postgresql://neondb_owner:npg_gPtB4VWLTf0r@ep-withered-shape-av6m9ydq-pooler.c-11.us-east-1.aws.neon.tech/neondb?sslmode=verify-full&connect_timeout=30",
       ssl: config.isProduction ? {
         rejectUnauthorized: false,
       } : false,
